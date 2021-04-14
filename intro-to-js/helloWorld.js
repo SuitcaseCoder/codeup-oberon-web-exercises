59a2c7ec7de018267b16e417ddab889fdaa8bb8d
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2021-04-14 12:30:33 -0500

    helloWordJS exercises

---
+++ intro-to-js/helloWorld.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// data types
+//
+// alert("I swear I won't make you write 'hello world'");
+//
+// alert("I hope my dogs don't get out again");
+
+
+// //  MINI EXERCISE
+// //TODO: I demand a haiku! Using alert, write a haiku!
+// //   -> The entire poem consists of just three lines, with 17 syllables in total.
+// //    -> The first line is 5 syllables.
+// //    -> The second line is 7 syllables.
+// //    -> haha The third line is 5 syllables.
+
+// LESSON EXERCISE
+// TODO: Create an html file on the project root called external_js.html
+// TODO: Create a JavaScript file inside of js/ called external.js
+// TODO: Link external.js to external_js.html by using <script src="external.js"></script>
+//  -> Be sure the script tag is inside the body tag of the html
+// TODO: Use the alert() function to show a message that says "Welcome to my Website!"
+// TODO: Use prompt() to ask for the user's favorite color.
+//  -> Assign the result to a variable and use its value to alert a message that shows the user's favorite color
+//  -> ie: "blue" -> "Your favorite color is: blue"
+// TODO: Use an if statement to see if that color is also your favorite color
+//  -> if it is your favorite as well, then show a special message for the user
+//  -> otherwise, show the normal message
